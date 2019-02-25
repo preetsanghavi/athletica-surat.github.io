e2a744408b216c8d24a3a44efefaf4ce254289a3
preetsanghavi <s.preet.sanghavi@fountainheadschools.org>
2019-02-25 21:24:38 +0530

    Athletica Website

---
+++ html/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Our products are manufactured using premium
+    optimum quality dyes & other material that are obtained from reputed vendors of
+    the market. Available in plethora of colors, sizes, shades and textures, these
+    products are used for sewing, knitting, crocheting, embroidery works, weaving
+    and rope-making.">
+    <meta name="keywords" content="Perfect Textile Industries, Yarn , Dyeing , embroidery , textile , Surat">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+    <link rel="stylesheet" href="css/index.css">
+
+    <title>Leading Textile Company | Yarn Processing | Dyeing | Perfect Textile Industries</title>
+  </head>
+  <body>
+    <div class="header">
+      <a href="Home.php" class="logo">Perfect Yarns</a>
+        <div class="header-right">
+          <a href="Home.php">Home</a>
+          <a href="About.php">About Us</a>
+          <a href="Products.php">Products</a>
+          <a href="Services.php">Services</a>
+          <a class="active" href="Contact.php">Contact Us</a>
+        </div>
+      </div>
+
+      <div class="bg">
+        <div class="Maincontent">
+          <div class="StaticInfo">
+            <p>We are a renowned manufacturer, supplier and wholesaler of a wide range of
+                carpet yarn, fancy yarn, embroidery threads etc. Our products are widely
+                appreciated for their attractive colors, high tenacity and long lasting luster.
+            </p>
+          </div>
+        </div>
+
+        <div class="Sideimage">
+          <img style="margin:0px 100px 0px 0px"src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/spools-of-yarn-yali-shi.jpg" height="450">
+        </div>
+      </div>
+
+      <div class="map">
+        <h2>Find Us</h2> <br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1860.1343894843578!2d72.82371538982804!3d21.181479125078585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e6a2dc2a1ff%3A0x2484cfbc648e158f!2sJeevandip+Complex!5e0!3m2!1sen!2sin!4v1548516281295" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="form">
+        <h2> Get in touch</h2>
+        <div class="form-container">
+          <form action="/action_page.php">
+            <label for="fname">First Name</label>
+            <br>
+            <input type="text" name="firstname" placeholder="Your name.." size="1000">
+            <br>
+            <label for="lname">Last Name</label>
+            <br>
+            <input type="text" name="lastname" placeholder="Your last name..">
+            <br>
+            <label for="email">Email</label>
+            <br>
+            <input type="email" name="Email" placeholder="Email Address">
+            <br>
+            <label for="phnum">Phone Number</label>
+            <br>
+            <input type="text" name="phone number" placeholder="Phone Number">
+            <br>
+            <label for="subject">Subject</label>
+            <br>
+            <textarea name="subject" placeholder="Write something.." style="height:100px"></textarea>
+            <br>
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>
+
+</body>
+<?php
+include("footer.php");
+?>
+</html>
+
+
